d95c78fa7a89ddc35dc72319ecda9f2e4e19420b
Sizm12 <s.zeledon.cbe@gmail.com>
2022-04-25 18:40:09 +0000

    [ADD] Basic Module

---
+++ pos_action_button/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Dashboard',
+    'version': '1.0',
+    'summary': 'Dashboard button in POS screen',
+    'category': 'Point Of Sales',
+    'author': 'Somos Moa',
+    'maintainer': 'Somos Moa',
+    'company': 'Somos Moa',
+    #'depends': ['base', 'point_of_sale'],
+    'depends': ['base'],
+    #'qweb': ['static/src/views/action_button.xml'],
+    #'data': ['views/view.xml'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
